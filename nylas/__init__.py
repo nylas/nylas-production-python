a9f78325a0ad1f95819f5b027bcdd57d24b03f62
Christine Spang <christine@spang.cc>
2015-08-10 19:23:27 -0700

    Tweak top-level __init__.py to actually work

--- nylas/__init__.py
+++ nylas/__init__.py
@@ -2,8 +2,12 @@
 # __init__.py. Don't change it unless you want to introduce import bugs
 # based on install ordering.
 from pkgutil import extend_path
-from .client.client import APIClient
 
 # Allow out-of-tree submodules.
 __path__ = extend_path(__path__, __name__)
-__all__ = ['APIClient']
+
+try:
+    from nylas.client.client import APIClient
+    __all__ = ['APIClient']
+except ImportError:
+    pass
